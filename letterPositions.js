07411afcef5d638039efed9adcf2826d75590511
Gokul Krishna <gokursx.123@gmail.com>
2024-05-01 17:18:41 -0700

    letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,14 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: [" ${actual}] === [" ${expected} "]`);
+  } else {
+    console.log(`Assertion Failed: [" ${actual}] === [" ${expected} "]`);
+  }
+};
+
 const letterPositions = function (sentence) {
   const results = {};
   // logic to update results here
-  for (const letter of sentence) {
-    // inside the loop:
-   
-    if (results[letter]) {
-      results[letter] += sentence[letter];
-    } else {
-      results[letter] = sentence[letter];
-    }
+  let i =0;
+    for(let letter of sentence){
+      if(results[letter]){
+        results[letter].push(i)
+      } 
+      else{
+        results[letter] = [i]
+      }
+    i++
   }
   return results;
 };
+
+// Test Cases
+assertEqual(letterPositions("Deposit"){
+  d: [0],
+  e: [1],
+  p: [2],
+  o: [3],
+  s: [4],
+  i: [5],
+  t: [6]
+});
