2bb3f9868e8427928c2fa2be10e679aa97f7ff68
Gokul Krishna <gokursx.123@gmail.com>
2024-05-02 12:46:48 -0700

    Your commit message

---
+++ assertArraysEqual.js
@@ -0,0 +1,13 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1 === arr2) {
+    console.log("Arrays are equal");
+  } else {
+    console.log("Arrays are not equal");
+  }
+};
+
+assertArraysEqual ([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual ([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual (["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual (["1", "2", "3"], ["1", "2", 3]); // => false
