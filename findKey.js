0d9ddf8cedee22d869f95f5d07526923e854e753
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-03 15:51:07 -0700

    Create findKey.js

---
+++ findKey.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: [" ${actual}] === [" ${expected} "]`);
+  } else {
+    console.log(`Assertion Failed: [" ${actual}] === [" ${expected} "]`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+
+    else {
+      return undefined;
+    }
+  }
+};
