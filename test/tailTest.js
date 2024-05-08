a5105c604b1c8dd362f66a0ffa0f53aba21600da
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-08 15:01:48 -0700

    Update tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,18 @@
 // tailTest.js
 
-const assertEqual = require("../assertEqual");
-const tail = require("../tail");
+//Importing chai 
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+//Uses deepEqual from Chai library
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+})
+
+// const assertEqual = require("../assertEqual");
+//const tail = require("../tail");
 
 // Testing code
 assertEqual(tail([5, 6, 7]), 5);
