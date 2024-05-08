7e1285d5083eaa8fa8c04c3aad470896c2d5d79e
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-08 14:50:14 -0700

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: [" ${actual}] === [" ${expected} "]`);
+    return `Assertion Passed: [" ${actual}] === [" ${expected} "]`;
   } else {
-    console.log(`Assertion Failed: [" ${actual}] !== [" ${expected} "]`);
+    return `Assertion Failed: [" ${actual}] !== [" ${expected} "]`;
   }
 };
 
