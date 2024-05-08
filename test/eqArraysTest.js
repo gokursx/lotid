00f4ef32bc6cc64d069b2f20993cf8be2d8a5498
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-08 14:40:45 -0700

    Add files via upload

---
+++ test/eqArraysTest.js
@@ -0,0 +1,7 @@
+//eqArraysTest.js
+
+//Imports assertEqual and eqArrays modules
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
