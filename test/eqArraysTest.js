e8199f24f50986525feec6188a8f45ceaeaab24f
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-08 14:43:27 -0700

    Update eqArraysTest.js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,3 +5,5 @@ const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 4, 2]), false);
+assertEqual(eqArrays([1, 3, 3], [1, 4, 3]), true);
