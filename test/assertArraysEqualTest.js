852aff68f60301bd17b27855c094129019f0e423
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-08 14:48:51 -0700

    Add files via upload

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+//assertArraysEqualTest.js
+
+//Importing assertArraysEqual to the module
+const assertArraysEqual = require('../assertArraysEqual');
+
+//Test Cases
+assertArraysEqual([7, 2, 3], [1, 3]);
+assertArraysEqual([3, 5, 7], [3, 5, 7]);
+assertArraysEqual([1, 3, 3], [1, 3, 3]);
