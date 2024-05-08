9d4ead4bd4f8193dd0b42847d8275480859eb4c7
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-08 15:02:08 -0700

    Update tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -15,5 +15,5 @@ describe("#tail", () => {
 //const tail = require("../tail");
 
 // Testing code
-assertEqual(tail([5, 6, 7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(tail([5, 6, 7]), 5);
+//assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
